54367d9cdb59664c7cd9f38e5e1424e453d1387f
Danial Amin <danialamin489@gmail.com>
2024-03-16 18:52:08 +0500

    fixing deployment issues 11

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -3,7 +3,9 @@ import { Await, Link, defer, useLoaderData } from "react-router-dom"
 export const searchLoader = async ({request, params}) => {
   const url = new URL(request.url)
   const searchParams = url.searchParams
-  const res = await fetch(`https://real-estate-marketplace-mern-server-nu.vercel.app/get/get?searchTerm=${searchParams.get("searchTerm")}`)
+  const res = await fetch(`https://real-estate-marketplace-mern-server-nu.vercel.app/get/get?searchTerm=${searchParams.get("searchTerm")}`, {
+    accessControlAllowOrigin: true
+  })
   const data = res.json()
 
   return defer({data: data})
