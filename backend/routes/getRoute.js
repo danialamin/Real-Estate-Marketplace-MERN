ba04a35585ced26c1aac00b4f9a3fccbcbf2477d
danialamin <110622257+danialamin@users.noreply.github.com>
2024-03-17 17:15:53 +0500

    Delete backend/routes directory

--- backend/routes/getRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const {getController} = require('../controllers/getController')
-
-const Router = express.Router()
-
-Router.get('/get', getController)
-
-module.exports = Router
